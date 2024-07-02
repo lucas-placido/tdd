98173d353d59731cd635318d40ddb582cc74c095
lucas-placido <lucas.cplacido@hotmail.com>
2024-07-02 00:14:47 -0300

    testing parametrize

---
+++ tdd/main.py
@@ -0,0 +1,18 @@
+def create_user_id(users: dict[int, str]) -> int:
+    if not users:
+        return 1
+    return max(users.keys()) + 1
+
+
+def create_user(user_name: str, dict_base: dict[int, str]) -> dict[int, str]:
+    new_user_id = create_user_id(dict_base)
+    dict_base[new_user_id] = user_name
+    return dict_base
+
+
+def remove_user():
+    pass
+
+
+def list_users():
+    pass
